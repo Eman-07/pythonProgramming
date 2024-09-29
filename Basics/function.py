bead52b99aed32a178f322c65332062790eabbea
Muhammad Eman <memanmaan44@gmail.com>
2024-09-29 16:58:31 +0500

    Added basic programs

---
+++ Basics/function.py
@@ -0,0 +1,11 @@
+def hello():
+    print("this hello world in printed by funtion call")
+    print("python is interesting")
+
+def greet(person):
+    print(" hello " + person)
+    print("Como Estas? " + person)
+
+hello()
+
+greet("john")
